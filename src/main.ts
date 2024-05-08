9c4920711ad5ea90b9202c858b18a7e5b8f21f66
ziggornif <matthieudrouian@gmail.com>
2024-05-08 19:27:45 +0200

    feat: improve error cases

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,28 @@ window.onload = async function () {
   const extruderCalcForm = document.getElementById("extruder-calc");
   extruderCalcForm?.addEventListener('submit', async (event) => {
     event.preventDefault();
+    const errorHelper = document.getElementById("invalid-helper");
+    errorHelper!.innerHTML = "";
     const extrudedInput = document.getElementById("extruded") as HTMLInputElement;
     const extruded = parseFloat(extrudedInput.value);
+    if (isNaN(extruded)) {
+      errorHelper!.innerHTML = "Extruded length must be a number"
+      return;
+    }
     const stepsInput = document.getElementById("current-steps") as HTMLInputElement;
     const currentSteps = parseFloat(stepsInput.value);
+    if (isNaN(currentSteps) ) {
+      errorHelper!.innerHTML = "Current steps/mm extruder configuration must be a number"
+      return;
+    }
     const newValue = calibrateExtruder(currentSteps, extruded);
     const extrudedResult = document.getElementById("extruder-result");
+    extrudedResult!.innerHTML = "";
     const pResult = document.createElement('p');
     pResult.innerHTML = `Your new configuration must be <strong>${newValue} steps/mm<strong>.`;
     extrudedResult?.append(pResult);
     extrudedResult?.scrollIntoView();
+    extrudedInput!.value = "";
+    stepsInput!.value = "";
   });
 }
